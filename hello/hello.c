2009a2a775f4c2389f3a8ad14e86aaf44e8bd534
CSapp5 <CSapp5@husker.unl.edu>
2020-08-20 14:38:12 -0500

    Update hello.c
    Added my name and major

--- hello/hello.c
+++ hello/hello.c
@@ -1,18 +1,21 @@
 /**
  * Author Reese McReynolds
  *          rmcreynolds3@husker.unl.edu
- * 
+ *
  * Date: 2020/20/8
- * 
+ *
  * A simple hello world program in C
- * 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("Reese McReynolds!\n");
-    pringf("Computer Science!\n");
+    printf("Computer Science!\n");
+    printf("Clay Sapp!\n", );
+    printf("Computer Science!\n", );
+
     return 0;
 }
