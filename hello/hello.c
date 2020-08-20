6445a448109c068628c1ef74d4e1fc77267efe4f
rmcreynolds3 <69827596+rmcreynolds3@users.noreply.github.com>
2020-08-20 13:51:46 -0700

    Initial commit

---
+++ hello/hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author Reese McReynolds
+ *          rmcreynolds3@husker.unl.edu
+ * 
+ * Date: 2020/20/8
+ * 
+ * A simple hello world program in C
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Reese McReynolds!\n");
+    return 0;
+}
